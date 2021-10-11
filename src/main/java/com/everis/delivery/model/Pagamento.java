2166c55a58a7006b906ebe24663215785b11f43d
Liudimilla Silva <liudimillasilva@gmail.com>
2021-10-11 12:05:28 -0300

    Cominnt "model/pagamento"

---
+++ src/main/java/com/everis/delivery/model/Pagamento.java
@@ -0,0 +1,62 @@
+package com.everis.delivery.model;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import javax.persistence.*;
+
+@EntityScan
+@Table(name = "pagamento")
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id" )
+    private Integer id;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "conta_id", referencedColumnName = "id" )
+    private Conta origem;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "conta_id", referencedColumnName =  "id" )
+    private Conta destino;
+
+    @Column(name = "valorPagamento")
+    private double valorPagamento;
+
+    public Pagamento (Conta origem, Conta destino, double valorPagamento) {
+            super();
+            this. origem = origem;
+            this. destino = destino;
+            this. valorPagamento = valorPagamento;
+            }
+
+            public Integer getId () {
+            return id;
+            }
+            public void setId(Integer id) {
+            this. id = id;
+    }
+
+            public Conta getOrigem() {
+            return origem;
+    }
+
+    public void setOrigem(Conta origem) {
+        this.origem = origem;
+    }
+
+    public Conta getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Conta destino) {
+        this.destino = destino;
+    }
+
+    public double getValorPagamento() {
+        return valorPagamento;
+    }
+
+    public void setValorPagamento(double valorPagamento ) {
+        this.valorPagamento = valorPagamento;
+    }
+
+}
